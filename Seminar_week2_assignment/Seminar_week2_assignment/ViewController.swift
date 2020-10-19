d793e828a1593934b62b47e1dcdf316b446882b3
june <brainyjun@gmail.com>
2020-10-19 13:23:34 +0900

    Update : Add Zeplin image and init scroll view setting

---
+++ Seminar_week2_assignment/Seminar_week2_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar_week2_assignment
+//
+//  Created by june on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
