fc61fcf8850795a6f03c985ee40946cd6848f797
june <brainyjun@gmail.com>
2020-10-19 16:02:12 +0900

    Update : Add scroll view delegate and to top button

--- Seminar_week2_assignment/Seminar_week2_assignment/ViewController.swift
+++ Seminar_week2_assignment/Seminar_week2_assignment/ViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var profileWriteButton: UIButton!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +20,23 @@ class ViewController: UIViewController {
     }
     
     func setViewAttr() {
+        scrollView.delegate = self
         profileWriteButton.layer.cornerRadius = 5
     }
 
-
+    @IBAction func topButtonClicked(_ sender: UIButton!) {
+        scrollView.scrollRectToVisible(CGRect(x:0, y: 0, width: 1, height: 1), animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (scrollView.contentOffset.y <= 0){
+            topButton.isHidden = true
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        topButton.isHidden = false
+    }
+    
 }
 
