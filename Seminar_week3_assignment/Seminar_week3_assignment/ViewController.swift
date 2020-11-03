aa08807474eba1968347e107950f5f28bf963844
june <brainyjun@gmail.com>
2020-11-03 16:40:51 +0900

    UPDATE : Add week3 project

---
+++ Seminar_week3_assignment/Seminar_week3_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar_week3_assignment
+//
+//  Created by june on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
