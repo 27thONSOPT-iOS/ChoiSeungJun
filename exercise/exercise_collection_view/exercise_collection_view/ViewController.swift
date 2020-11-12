b757cf829dddcf50c1cbca1be271e627e25a83dd
june <brainyjun@gmail.com>
2020-11-12 19:19:44 +0900

    UPDATE : Refactor package

---
+++ exercise/exercise_collection_view/exercise_collection_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise_collection_view
+//
+//  Created by june on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
